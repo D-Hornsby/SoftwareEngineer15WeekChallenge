ce108f4720ed1c138c309f984599ef02d27d72b5
D-Hornsby <denahornsby@yahoo.com>
2021-03-26 22:14:53 -0400

    fixing files.

---
+++ Mod1/Week1/Week1 labs/W1D4ML/AverageNum.js
@@ -0,0 +1,31 @@
+//find the average of (4, 10, 22, -30, 55)
+//Find the average of ( 4, 10, 22, -30, 55); Print in console
+
+// var num1, num2, num3, num4, num5;
+//   num1=4;
+//   num2=10;
+//   num3=22;
+//   num4=-30;
+//   num5=55;
+
+// var avg = (num1 + num2 + num3 + num4 + num5)/5;
+// console.log(avg);
+
+
+
+//Find the average of ( 68, 7, -22, 9, 100); Print in console
+
+var num1, num2, num3, num4, num5;
+  num1=68;
+  num2=7;
+  num3=-22;
+  num4=9;
+  num5=100;
+
+var avg = (num1 + num2 + num3 + num4 + num5)/5;
+console.log(avg);
+
+
+//to console log in you terminal put    node AverageNum.js
+//or node whateverfilename.whatever here
+
